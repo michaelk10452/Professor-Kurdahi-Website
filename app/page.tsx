406014e20752631264762c9e279076f1515806c7
michaelk10452 <mskurdah@uci.edu>
2026-01-20 23:47:32 -0800

    Updated photo

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
                     sizes="(max-width: 768px) 100vw, 33vw"
                     style={{ 
                       objectFit: 'cover', 
-                      objectPosition: 'center 50%' 
+                      objectPosition: 'center 75%' 
                     }}
                     className="transition-transform duration-300 hover:scale-102"
                   />
