03bfc8e9c130b74d09361aa330d603572f19d5c3
michaelk10452 <mskurdah@uci.edu>
2026-01-21 00:28:52 -0800

    Updated bio

--- app/page.tsx
+++ app/page.tsx
@@ -55,11 +55,11 @@ export default function Home() {
                 </h1>
                 <div className="space-y-6 text-gray-100">
                   <p className="text-lg">
-                    A Distinguished Professor who has been in the{" "}
+                    A Distinguished Professor in the{" "}
                     <a href="https://engineering.uci.edu/dept/eecs" className="text-white hover:text-gray-200 underline underline-offset-4 decoration-1">
                       Department of Electrical Engineering and Computer Science
                     </a>{" "}
-                    at UC Irvine since 1987, he currently serves as the Director of the{" "}
+                    UC Irvine, he has been a faculty member since 1987 and currently serves as Director of the{" "}
                     <a href="https://cecs.uci.edu" className="text-white hover:text-gray-200 underline underline-offset-4 decoration-1">
                       Center for Embedded and Cyber-Physical Systems (CECS)
                     </a>.{" "}
@@ -80,7 +80,7 @@ export default function Home() {
                     </a>.
                   </p>
                   <p className="text-lg">
-                    An IEEE and AAAS Fellow, Prof. Kurdahi earned his M.S. & Ph.D. in Computer Engineering from USC and his undergraduate degree from the American University of Beirut. He remains passionate about advancing engineering research and education and preparing students for the rapidly evolving challenges of embedded and cyber-physical systems.
+                    An IEEE and AAAS Fellow, Prof. Kurdahi earned his M.S. & Ph.D. in Computer Engineering from USC and his undergraduate degree from the American University of Beirut. He remains passionate about advancing engineering research and education as well as preparing students for the rapidly evolving challenges of embedded and cyber-physical systems.
                   </p>
                 </div>
               </div>
